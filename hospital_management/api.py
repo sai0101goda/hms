a204a1b6994ad9b2ba03419c32e8b6ab914954dd
savartha <saisrinivas.vartha@gmail.com>
2024-12-20 11:51:24 +0530

    modify api to return list view and list filter

--- hospital_management/api.py
+++ hospital_management/api.py
@@ -3,7 +3,7 @@ import frappe
 @frappe.whitelist(allow_guest=True)
 def get_all_doctype_fields(doctype):
     doctype_name = doctype
-    fields = frappe.get_all('DocField', filters={'parent': doctype_name}, fields=["fieldname", "fieldtype", "label","options","idx","reqd"])
+    fields = frappe.get_all('DocField', filters={'parent': doctype_name}, fields=["fieldname", "fieldtype", "label","options","idx","reqd","in_list_view","in_filter"])
 
     field_data = []
     for field in fields:
@@ -13,7 +13,9 @@ def get_all_doctype_fields(doctype):
             "label": field['label'],
             "option": field['options'],
             "idx": field['idx'],
-            "idx": field['reqd']
+            "reqd": field['reqd'],
+            "in_list_view": field['in_list_view'],
+            "in_filter": field['in_filter']
         })
 
     frappe.local.response.update({
